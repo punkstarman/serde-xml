e003369229f34a33ebc57d0baed570836e8db9c2
William Bartlett <w.bartlett@treeptik.fr>
2018-11-29 11:07:24 +0100

    Remove commented code

--- src/error.rs
+++ src/error.rs
@@ -15,19 +15,13 @@ pub struct Error(Box<ErrorImpl>);
 #[derive(Debug)]
 pub enum ErrorImpl {
     Message(String),
-    //Io(io::Error),
     Reader(reader::Error),
     ParseIntError(ParseIntError),
-    //Writer(writer::Error),
 }
 
 pub fn with_message(s: String) -> Error {
     Error(Box::new(ErrorImpl::Message(s)))
 }
-
-//pub fn io(err: io::Error) -> Error {
-//    Error(Box::new(ErrorImpl::Io(err)))
-//}
 
 pub fn parse_int(err: ParseIntError) -> Error {
     Error(Box::new(ErrorImpl::ParseIntError(err)))
@@ -37,18 +31,12 @@ pub fn reader(err: reader::Error) -> Error {
     Error(Box::new(ErrorImpl::Reader(err)))
 }
 
-// pub fn writer(err: writer::Error) -> Error {
-//     Error(Box::new(ErrorImpl::Writer(err)))
-//}
-
 impl Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self.0 {
             ErrorImpl::Message(ref m) => write!(f, "{}", m),
-            //ErrorImpl::Io(ref err) => write!(f, "{}", err),
             ErrorImpl::Reader(ref err) => write!(f, "{}", err),
             ErrorImpl::ParseIntError(ref err) => write!(f, "{}", err),
-            //ErrorImpl::Writer(ref err) => write!(f, "{}", err),
         }
     }
 }
