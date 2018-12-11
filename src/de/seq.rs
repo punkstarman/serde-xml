458ffed6309d33aea08c54753b64da7693a5a2db
William Bartlett <w.bartlett@treeptik.fr>
2018-12-11 13:49:22 +0100

    refactor

--- src/de/seq.rs
+++ src/de/seq.rs
@@ -3,17 +3,18 @@ use std::io::Read;
 use xml::reader::XmlEvent;
 
 use super::Deserializer;
-use super::super::error::{Error, Result};
+use super::super::error::{self, Error, Result};
 
 pub struct SeqAccess<'a, R: 'a + Read> {
     de: &'a mut Deserializer<R>,
-    tag_name: Option<String>,
+    tag_name: String,
     first: bool,
 }
 
 impl<'a, R: 'a + Read> SeqAccess<'a, R> {
     pub fn new(de: &'a mut Deserializer<R>) -> Result<Self> {
-        let tag_name = de.current_tag();
+        let tag_name = de.current_tag()
+            .ok_or(error::with_message("expected current tag".to_string()))?;
         Ok(SeqAccess { de, tag_name, first: true })
     }
 }
@@ -30,12 +31,9 @@ impl<'de, 'a, R: 'a + Read> serde::de::SeqAccess<'de> for SeqAccess<'a, R> {
             self.first = false;
             seed.deserialize(&mut *self.de).map(Some)
         } else {
-            if let Some(ref t) = self.tag_name {
-                self.de.end_tag(&t)?;
-            }
-            
-            match (self.de.peek()?.clone(), self.tag_name.as_ref()) {
-                (XmlEvent::StartElement { ref name, .. }, Some(t)) if &name.local_name == t => {
+            self.de.end_tag(&self.tag_name)?;
+            match self.de.peek()?.clone() {
+                XmlEvent::StartElement { ref name, .. } if name.local_name == self.tag_name => {
                     self.de.start_tag()?;
                     let v = seed.deserialize(&mut *self.de)?;
                     Ok(Some(v))
